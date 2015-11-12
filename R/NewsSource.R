e659a8c1cc4168afeea4e88125eb86f9fa665385
sjkiss <sjkiss@gmail.com>
2015-11-12 12:37:35 -0500

    tm.newsstand
    Plugin for the tm package to read in NewsStand articles

--- R/NewsSource.R
+++ R/NewsSource.R
@@ -2,7 +2,7 @@ NewsSource<-function(x, encoding = "UTF-8") {
   # Use filename 'x' to read the file and split it
   # into a character vector called 'content' with one item
   # per document
-  content<-readLines(x)
+  content<-readLines(x, warn=FALSE)
   content<-content[1:(length(content)-6)]
   ###The following are lines of usually superfluous information that are placed between the full text and Title:.  They are annoying because they are not always attached to every article, making them useless as markers to end the full text section via grep in readNews. If we did not delete them, they would end up in the full-text section of every corpus element.  They have to be deleted with the invert argument because grepping those lines that do not match ^Subject: via the -grep argument fails when there are none of these lines in a document. This has happened.
   #content<-   content[grep('^Credit:', content, invert=TRUE)]
